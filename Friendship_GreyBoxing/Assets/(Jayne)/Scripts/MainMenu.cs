30ec27c26905165f14ea9cc1b58e3b20a166a23a
Jayne <jaynegale@gmail.com>
2019-03-22 14:31:09 +1300

    My stuff before merge

---
+++ Friendship_GreyBoxing/Assets/(Jayne)/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public KeyCode pauseMenuKey = KeyCode.P;
+    public KeyCode quitMenuKey = KeyCode.Escape;
+    public KeyCode jumpMenuKey = KeyCode.Space;
+    public KeyCode RunMenuKey = KeyCode.LeftShift;
+    public KeyCode Run2MenuKey = KeyCode.RightShift;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Midnight");
+    }
+
+    public void ReallyQuit()
+    {
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+
+}
---
+++ Friendship_GreyBoxing/Assets/(Jayne)/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public KeyCode pauseMenuKey;
+    public static PauseMenu instance = null;
+    public GameObject pauseMenu;
+    private bool paused;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(pauseMenuKey))
+        {
+            print("pausingNow");
+            paused = !paused;
+        }
+
+        if (paused == true)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void GameResume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
