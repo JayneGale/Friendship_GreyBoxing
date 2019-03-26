ce7171b3a4f4b4e4d6cd2cff3912319a5038b7be
Jayne <jaynegale@gmail.com>
2019-03-26 15:34:17 +1300

    UI changes

---
+++ Friendship_GreyBoxing/Assets/(Jayne)/Prefabs/UIprefabs/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Midnight");
+    }
+
+    public void ReallyQuit()
+    {
+        if (Application.isEditor)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            print("Quit pressed, exiting Play mode");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+
+}
---
+++ Friendship_GreyBoxing/Assets/(Jayne)/Prefabs/UIprefabs/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    public void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
+        }
+    }
+
+    public void GameResume()
+    {
+        Debug.Log("ResumeGame Button pressed");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0;
+    }
+
+    public void ReallyQuit()
+    {
+        if (Application.isEditor)
+        {
+            print("Quit pressed, exiting Play mode");
+            UnityEditor.EditorApplication.isPlaying = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+}
+
